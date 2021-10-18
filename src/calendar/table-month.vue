1819b184911b096f048c4e712e703e9ca0c7a0fb
darts <jhonedarts@gmail.com>
2021-10-18 15:05:33 -0300

    start component

---
+++ src/calendar/table-month.vue
@@ -0,0 +1,104 @@
+<template>
+  <div :class="`${prefixClass}-calendar ${prefixClass}-calendar-panel-month`">
+    <div :class="`${prefixClass}-calendar-header`">
+      <icon-button type="left" @click="handleIconDoubleLeftClick"></icon-button>
+      <icon-button type="right" @click="handleIconDoubleRightClick"></icon-button>
+      <span :class="`${prefixClass}-calendar-header-label`">
+        <button
+          type="button"
+          :class="`${prefixClass}-btn ${prefixClass}-btn-text`"
+          @click="handlePanelChange"
+        >
+          {{ calendarYear }}
+        </button>
+      </span>
+    </div>
+    <div :class="`${prefixClass}-calendar-content`">
+      <table :class="`${prefixClass}-table ${prefixClass}-table-month`" @click="handleClick">
+        <tr v-for="(row, i) in months" :key="i">
+          <td
+            v-for="(cell, j) in row"
+            :key="j"
+            :data-month="cell.month"
+            class="cell"
+            :class="getCellClasses(cell.month)"
+          >
+            <div>{{ cell.text }}</div>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import { chunk } from '../util/base';
+import IconButton from './icon-button.vue';
+import { getLocale } from '../locale';
+import { setYear } from '../util/date';
+
+export default {
+  name: 'TableMonth',
+  components: { IconButton },
+  inject: {
+    getLocale: {
+      default: () => getLocale,
+    },
+    prefixClass: {
+      default: 'mx',
+    },
+  },
+  props: {
+    calendar: {
+      type: Date,
+      default: () => new Date(),
+    },
+    getCellClasses: {
+      type: Function,
+      default: () => [],
+    },
+  },
+  computed: {
+    calendarYear() {
+      return this.calendar.getFullYear();
+    },
+    months() {
+      const locale = this.getLocale();
+      const monthsLocale = locale.months || locale.formatLocale.monthsShort;
+      const months = monthsLocale.map((text, month) => {
+        return { text, month };
+      });
+      return chunk(months, 3);
+    },
+  },
+  methods: {
+    handleIconDoubleLeftClick() {
+      this.$emit(
+        'changecalendar',
+        setYear(this.calendar, v => v - 1),
+        'last-year'
+      );
+    },
+    handleIconDoubleRightClick() {
+      this.$emit(
+        'changecalendar',
+        setYear(this.calendar, v => v + 1),
+        'next-year'
+      );
+    },
+    handlePanelChange() {
+      this.$emit('changepanel', 'year');
+    },
+    handleClick(evt) {
+      let { target } = evt;
+      if (target.tagName.toUpperCase() === 'DIV') {
+        target = target.parentNode;
+      }
+      const month = target.getAttribute('data-month');
+      if (month) {
+        this.$emit('select', parseInt(month, 10));
+      }
+    },
+  },
+};
+</script>
